0bafe8eb8d0292cf60ea1b6a0b24284ad7c286df
CaptainK123 <123463421+CaptainK123@users.noreply.github.com>
2023-01-24 21:48:23 +0200

    Update settings.py

--- settings.py
+++ settings.py
@@ -3,6 +3,8 @@ api_secret = ''
 discord_autho = ''
 testnet1= True
 discord_channel = ''
+#LIMIT OR MARKET
+market_pos = 'LIMIT'
 #% of balance to use on a trade should be between 0.05 and 0.098
 balance_margin = 0.05
 
