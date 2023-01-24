95c686eb6109076f94d53bd75536a5676630f680
CaptainK123 <123463421+CaptainK123@users.noreply.github.com>
2023-01-24 21:47:56 +0200

    Add files via upload

--- requestsdiscord.py
+++ requestsdiscord.py
@@ -2,8 +2,9 @@ import requests
 import json
 import time
 from decimal import Decimal
-from settings import api_key,api_secret,discord_autho,balance_margin,testnet1,discord_channel
+from settings import api_key,api_secret,discord_autho,balance_margin,testnet1,discord_channel,market_pos
 from binance.client import Client
+import re
 
 usdt_balance = 0
 client = Client(api_key=api_key, api_secret=api_secret,testnet = testnet1)
@@ -27,7 +28,10 @@ def retrieve_last_message(channelid):
         elif "Stoploss:" in line:
             stoploss = line.split(":")[1].strip().split("(")[0]
 
-    
+    match = re.search("Entry: ([\d.]+)", content)
+    if match:
+        entry_price = float(match.group(1))
+        
     
     for line in lines:
         long_split = line.split("BUY/LONG")
@@ -40,8 +44,8 @@ def retrieve_last_message(channelid):
             break
 
         
-    return content,instrument,target,stoploss,position
-content, instrument, target, stoploss,position = retrieve_last_message(discord_channel)
+    return content,instrument,target,stoploss,position,entry_price
+content, instrument, target, stoploss,position,entry_price = retrieve_last_message(discord_channel)
 
 def send_message_to_user(message):
     # code to send message to user
@@ -56,16 +60,19 @@ while True:
             break
         print(usdt_balance)
     
-    content, instrument, target, stoploss, position = retrieve_last_message(discord_channel)
+    content, instrument, target, stoploss, position, entry_price = retrieve_last_message(discord_channel)
     modified_string = instrument.replace("$", "") + "USDT"
     print(modified_string)
     key = "https://api.binance.com/api/v3/ticker/price?symbol=BTCUSDT"
     data = requests.get(key)  
     data = data.json()
     last_price = float(data['price'])
-    quantity = (usdt_balance * 0.15) / last_price
+    quantity = (usdt_balance * balance_margin) / last_price
     
     quantity = float(round(quantity, 2))
+    quantity_entry =(usdt_balance * balance_margin) / entry_price
+    quantity_entry = float(round(quantity, 2))
+
     print(stoploss)
     print(target)
     print(instrument)
@@ -73,15 +80,34 @@ while True:
     print(usdt_balance)
     print(quantity)
     print(position)
+    print(entry_price)
     if modified_string not in bought_symbols:
         target = float(target.split(" ")[0])
+
         if position == 'BUY/LONG':
-            buyorder=client.futures_create_order(symbol=modified_string,side='BUY',type='MARKET',quantity=quantity)     
-            print(buyorder)
+            if market_pos == 'MARKET':
+                buyorder=client.futures_create_order(symbol=modified_string,side='BUY',type='MARKET',quantity=quantity)     
+                print(buyorder)
+                buyorderlimit1=client.futures_create_order(symbol=modified_string,side='SELL',type='STOP_MARKET',stopPrice=stoploss,closePosition='true') 
+                sellorderlimit1=client.futures_create_order(symbol=modified_string,side='SELL',type='TAKE_PROFIT_MARKET',stopPrice=target,closePosition='true') 
+            elif market_pos == 'LIMIT':
+                buyorder=client.futures_create_order(symbol=modified_string,side='BUY',type='LIMIT',quantity=quantity_entry,price = entry_price,timeInForce='GTC')       
+                print(buyorder)
+                buyorderlimit1=client.futures_create_order(symbol=modified_string,side='SELL',type='STOP_MARKET',stopPrice=stoploss,closePosition='true') 
+                sellorderlimit1=client.futures_create_order(symbol=modified_string,side='SELL',type='TAKE_PROFIT_MARKET',stopPrice=target,closePosition='true')
         elif position == 'SELL/SHORT':
-            buyorder=client.futures_create_order(symbol=modified_string,side='SELL',type='MARKET',quantity=quantity)  
-            print(buyorder)
-              
+            if market_pos == 'MARKET':
+                buyorder=client.futures_create_order(symbol=modified_string,side='SELL',type='MARKET',quantity=quantity)  
+                print(buyorder)
+                buyorderlimit=client.futures_create_order(symbol=modified_string,side='BUY',type='STOP_MARKET',stopPrice=stoploss,closePosition='true') 
+                sellorderlimit=client.futures_create_order(symbol=modified_string,side='BUY',type='TAKE_PROFIT_MARKET',stopPrice=target,closePosition='true') 
+            elif market_pos == 'LIMIT': 
+                sellorder=client.futures_create_order(symbol=modified_string,side='SELL',type='LIMIT',quantity=quantity_entry,price= entry_price,timeInForce='GTC')       
+                print(sellorder)
+                buyorderlimit=client.futures_create_order(symbol=modified_string,side='BUY',type='STOP_MARKET',stopPrice=stoploss,closePosition='true') 
+                sellorderlimit=client.futures_create_order(symbol=modified_string,side='BUY',type='TAKE_PROFIT_MARKET',stopPrice=target,closePosition='true')
+ 
+
         
 
 
