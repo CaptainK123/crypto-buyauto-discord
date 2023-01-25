b7114abe70529ff44acc48e504d1546e0523e7c4
CaptainK123 <123463421+CaptainK123@users.noreply.github.com>
2023-01-25 12:15:51 +0200

    Update settings.py

--- settings.py
+++ settings.py
@@ -6,5 +6,7 @@ discord_channel = ''
 #LIMIT OR MARKET
 market_pos = 'LIMIT'
 #% of balance to use on a trade should be between 0.05 and 0.098
-balance_margin = 0.05
+#Тhe minimum purchase for one transaction must be over 5$ if it is less it will not go through you can change the margin from settings.
+#If your balance is $100 and you want per signal to be 5% of your balance, margin should be 0.06 
+balance_margin = 0.06
 
